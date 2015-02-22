d2d0311893d633e80b953e3d80d4d5ba78184f92
Chan Cho <verakorvo@gmail.com>
2015-02-21 19:41:44 -0800

    deleted missed old main directory

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from .views import home
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'bgtrbl.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', home, name='home'),
-)
