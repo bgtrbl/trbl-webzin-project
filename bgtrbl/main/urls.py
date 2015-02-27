0997ccba44cc3de9514450e0a2b79ecdcaa621eb
Chan Cho <verakorvo@gmail.com>
2015-02-27 00:01:40 -0800

    changed flatpage url

--- bgtrbl/main/urls.py
+++ bgtrbl/main/urls.py
@@ -5,7 +5,7 @@ from .views import home, allauthTest, profileView
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^allauth_test/', allauthTest, name='allauth_test'),
-    url(r'^(?P<url>.*)$', 'django.contrib.flatpages.views.flatpage', name='flatpage'),
+    url(r'^flatpage/(?P<url>.*)$', 'django.contrib.flatpages.views.flatpage', name='flatpage'),
 )
 
 # url(r'^pages/(?P<url>).*$', include('django.contrib.flatpages.urls')),
