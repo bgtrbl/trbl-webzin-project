e72f244fa537fcbce06a3952d28dc70078379fdb
Chan Cho <verakorvo@gmail.com>
2015-03-03 02:32:27 -0800

    userprofile main 에서 apps.userprofile 로 이동
    urls 수정, settings.py 변경

---
+++ bgtrbl/apps/userprofile/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+
+from django.views.generic.detail import DetailView
+
+from .models import UserProfile
+from .forms import UserProfileModelForm
+
+
+class UserProfileDetail(DetailView):
+    model = UserProfile
+    template_name = 'main/userprofile_detail.html'
+
+
+def editUserProfile(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    form = UserProfileModelForm(instance=user_profile)
+    return render(request, 'main/edit_userprofile.html', {'form': form})
+
+
+# login required
+def updateUserProfile(request):
+    if request.method == 'POST':
+        user_profile = UserProfile.objects.get(user=request.user)
+        form = UserProfileModelForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            user_profile = form.save(commit=False)
+            user_profile.user = request.user
+            user_profile.save()
+            return redirect('my_userprofile')
+        # @todo url namespace: main
+        return redirect('edit_userprofile')
+    return redirect('main:home')
+
+
+# view that displays user profile
+# login required
+def myUserProfile(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    return render(request, 'main/my_userprofile.html', {'user_profile': user_profile})
