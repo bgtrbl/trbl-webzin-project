c7a59c7afc787dea86c07d444da93733633833bc
Chan Cho <verakorvo@gmail.com>
2015-03-03 03:25:55 -0800

    userprofile: add admin model

--- bgtrbl/apps/userprofile/admin.py
+++ bgtrbl/apps/userprofile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "dob",)
+
+
+admin.site.register(models.UserProfile, UserProfileAdmin)
