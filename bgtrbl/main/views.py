eb4306c0c192b9ea0bb8daa19ab28baba5032a05
Hyun <znzlznzk@gmail.com>
2015-03-05 09:19:54 -0800

    magazin front page 그리드 추가, forum front page에 write 버튼

--- bgtrbl/main/views.py
+++ bgtrbl/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from django.views.generic import DetailView, ListView
-from bgtrbl.apps.trblcms.models import Article, Category
+from bgtrbl.apps.trblcms.models import Category, Article
 
 def home(request):
     return render(request, 'main/home.html', {})
@@ -12,14 +12,22 @@ def allauthTest(request):
     return render(request, 'main/allauth_test.html', {})
 
 class magazine(ListView):
-    model = Article
+    model = Category 
     template_name = "front_magazine.html" 
-    queryset = Article.objects.filter(category= Category.objects.get(pk=2))
+    context_object_name = "category"
+    queryset = Category.objects.filter(parent=Category.objects.filter(title='Magazin'))
+            
+    def get_context_data(self, **kwargs):
+        context = super(magazine, self).get_context_data(**kwargs)
+        for i in self.queryset: 
+            context[i.title] = Article.objects.filter(category=i)
+        return context
+    
 
 class forum(ListView):
     model = Article
     template_name = "front_forum.html"
-    queryset = Article.objects.filter(category= Category.objects.get(pk=3))[:20]
+    queryset = Article.objects.filter(category=Category.objects.get(pk=3))[:20]
 
 
     
