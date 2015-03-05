fc0db810c548509a044931d0d98c7469cb91a0b6
Chan Cho <verakorvo@gmail.com>
2015-03-05 05:31:03 -0800

    upgrade wikiscrap - 엔하위키 지원
    리소스 추가 고려해서 변경

--- helpers/wikiscrap.py
+++ helpers/wikiscrap.py
@@ -2,26 +2,60 @@ import requests
 from bs4 import BeautifulSoup
 
 
-RANDOM_ARTICLE_URL = {
-        'ko': 'http://ko.wikipedia.org/wiki/특수:임의문서',
-        'en': 'http://en.wikipedia.org/wiki/Special:Random',
-        'ja': 'http://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA',
-        'fr': 'http://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard',
-        'io': 'http://io.wikipedia.org/wiki/Specala:HazardaPagino',
+# resource settings: lang -> url, pars
+RESOURCES = {
+        'ko': {
+            'url': lambda : 'http://ko.wikipedia.org/wiki/특수:임의문서',
+            'pars': lambda x: pars_wiki(x)},
+        'en': {
+            'url': lambda : 'http://en.wikipedia.org/wiki/Special:Random',
+            'pars': lambda x: pars_wiki(x)},
+        'ja': {
+            'url': lambda : 'http://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA',
+            'pars': lambda x: pars_wiki(x)},
+        'fr': {
+            'url': lambda : 'http://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard',
+            'pars': lambda x: pars_wiki(x)},
+        'io': {
+            'url': lambda : 'http://io.wikipedia.org/wiki/Specala:HazardaPagino',
+            'pars': lambda x: pars_wiki(x)},
+
+        'enha': {
+            'url': lambda : "https://mirror.enha.kr"+ \
+                    BeautifulSoup(requests.get("https://mirror.enha.kr/random").text).meta.attrs['content'].split('=')[-1],
+            'pars': lambda x: pars_enha(x)},
         }
-LANGS = list(RANDOM_ARTICLE_URL.keys())
+
+
+LANGS = list(RESOURCES.keys())
+
 
 def get_random_doc(lang):
-    resp = requests.get(RANDOM_ARTICLE_URL[lang])
-    soup =  BeautifulSoup(resp.text)
-    title = soup.find(id='firstHeading').string
-    body = soup.find(id='mw-content-text')
+    resp = requests.get(RESOURCES[lang]['url']())
+    result =  RESOURCES[lang]['pars'](BeautifulSoup(resp.text))
+    result['lang_title'] = '[{}] {}'.format(lang, result['title'])
+    return result
+
+
+# Custom Parsers
+
+def pars_wiki(doc):
+    title = doc.find(id='firstHeading').string
+    body = doc.find(id='mw-content-text')
     body.noscript.extract()
-    tags = [ _.replace(" ", "_") for _ in soup.find(id='mw-normal-catlinks').ul.strings ]
+    tags = [ _.replace(" ", "_") for _ in doc.find(id='mw-normal-catlinks').ul.strings ]
+    tags += 'wiki'
     return {
             'title': title,
-            'lang_title': "[{}] {}".format(lang, title),
             'body': body,
             'tags': tags,
             }
 
+def pars_enha(doc):
+    title = doc.h1.string
+    body = doc.article
+    return {
+            'title': title,
+            'body': body,
+            'tags': ['엔하'],
+            }
