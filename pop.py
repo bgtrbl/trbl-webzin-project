a13d1bfb17c3dd4fdbc17975c5c82f57ca2802f5
Chan Cho <verakorvo@gmail.com>
2015-03-04 22:47:34 -0800

    pop.py 대거 수정

--- pop.py
+++ pop.py
@@ -10,21 +10,34 @@ from django.contrib.auth.models import User
 from random import choice
 
 
-cats = (Category.objects.get(title='Wiki'),
-        Category.objects.get(title='Magazin'),
-        Category.objects.get(title='Forum'))
+def clear_article():
+    print('clearing {} articles...'.format(len(Article.objects.all())))
+    for i, v in enumerate(Article.objects.all()):
+        print(i)
+        v.delete()
+    print("done")
 
 
-def save_wiki(wikidoc):
-    article = Article.objects.create(title=wikidoc['lang_title'],
-            body=wikidoc['body'], user=choice(User.objects.all()),
-            category=choice(cats))
-    article.tags.add(*wikidoc['tags'])
-    return article
+def update_index():
+    print('updating search index...')
+    os.system('python3 manage.py update_index')
+    print("done")
 
-clear_article = lambda :[ (print(i), v.delete()) for i, v in enumerate(Article.objects.all()) ]
 
-if __name__ == '__main__':
+def wiki_scrap():
+    # category lists
+    CATS = (Category.objects.get(title='Wiki'),
+            Category.objects.get(title='Magazin'),
+            Category.objects.get(title='Forum'))
+
+    #helper function
+    def save_wiki(wikidoc):
+        article = Article.objects.create(title=wikidoc['lang_title'],
+                body=wikidoc['body'], user=choice(User.objects.all()),
+                category=choice(CATS))
+        article.tags.add(*wikidoc['tags'])
+        return article
+
     count = int(input('Article count: '))
 
     while True:
@@ -39,3 +52,29 @@ if __name__ == '__main__':
 
     print("Created Articles:")
     print(articles)
+    update_index()
+
+
+def help_msg():
+    msg = "Command List:\n"
+    msg += "  (총 {} 개 커맨드)\n\n".format(len(COMMANDS))
+    for k, v in COMMANDS.items():
+        msg += "  {} \n\t- {}\n".format(k, v['desc'])
+    print(msg)
+
+
+COMMANDS = {}
+COMMANDS['clear'] = {'func': clear_article, 'desc': '아티클 전체 삭제'}
+COMMANDS['index'] = {'func': update_index, 'desc': '서치 인덱싱'}
+COMMANDS['create'] = {'func': wiki_scrap, 'desc': 'Wikipidea 아티클 스크래핑'}
+COMMANDS['quit'] = {'func': lambda : exit(), 'desc': '종료'}
+COMMANDS['?'] ={'func': help_msg, 'desc': '도움말'}
+
+
+if __name__ == '__main__':
+
+    while True:
+        try:
+            COMMANDS[input('\n# ')]['func']()
+        except KeyError:
+            print("command not found - type '?' to see commands")
