345e570446f7d10bfc74e294cdeffdc84381caed
Chan Cho <verakorvo@gmail.com>
2015-03-06 10:33:24 -0800

    trblcms: minor change in template(avatar try out) main: Capitalize class based view names

--- bgtrbl/main/urls.py
+++ bgtrbl/main/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import home, allauthTest, magazine, forum
+from .views import home, allauthTest, Magazine, Forum
 
 
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^allauth_test/', allauthTest, name='allauth_test'),
     url(r'^flatpage/(?P<url>.*)$', 'django.contrib.flatpages.views.flatpage', name='flatpage'),
-    url(r'^magazine/', magazine.as_view(), name='magazine'), 
-    url(r'^forum/', forum.as_view(), name='forum'), 
+    url(r'^magazine/', Magazine.as_view(), name='magazine'),
+    url(r'^forum/', Forum.as_view(), name='forum'),
 
 )
 
--- bgtrbl/main/views.py
+++ bgtrbl/main/views.py
@@ -22,26 +22,26 @@ def allauthTest(request):
     return render(request, 'main/allauth_test.html', {})
 
 
-class magazine(ListView):
+class Magazine(ListView):
     model = Category
     template_name = "front_magazine.html"
     context_object_name = "category"
     queryset = Category.objects.filter(parent=Category.objects.filter(title='Magazin'))
 
     def get_context_data(self, **kwargs):
-        context = super(magazine, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         _add_modal_login_form(self.request.user, context)
         for i in self.queryset:
             context[i.title] = Article.objects.filter(category=i)
         return context
 
 
-class forum(ListView):
+class Forum(ListView):
     model = Article
     template_name = "front_forum.html"
     queryset = Article.objects.filter(category=Category.objects.get(pk=3))[:20]
 
     def get_context_data(self, **kwargs):
-        context = super(forum, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         _add_modal_login_form(self.request.user, context)
         return context
