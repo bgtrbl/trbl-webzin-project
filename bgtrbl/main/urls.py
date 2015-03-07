5d449cc7c3fd748d57567a60df87fa3df8eec98c
Chan Cho <verakorvo@gmail.com>
2015-03-07 04:52:32 -0800

    main: views Magazine(ListView) -> magazine(view function) Category.get_descendants 쓰는걸로 바꿈

--- bgtrbl/main/urls.py
+++ bgtrbl/main/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import home, allauthTest, Magazine, Forum
+from .views import home, allauthTest, Forum, magazine
 
 
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^allauth_test/', allauthTest, name='allauth_test'),
     url(r'^flatpage/(?P<url>.*)$', 'django.contrib.flatpages.views.flatpage', name='flatpage'),
-    url(r'^magazine/', Magazine.as_view(), name='magazine'),
+    url(r'^magazine/', magazine, name='magazine'),
     url(r'^forum/', Forum.as_view(), name='forum'),
 
 )
--- bgtrbl/main/views.py
+++ bgtrbl/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 from django.views.generic import DetailView, ListView
@@ -22,24 +22,16 @@ def allauthTest(request):
     return render(request, 'main/allauth_test.html', {})
 
 
-class Magazine(ListView):
-    model = Category
-    template_name = "front_magazine.html"
-    context_object_name = "category"
-    queryset = Category.objects.filter(parent=Category.objects.filter(title='Magazin'))
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        _add_modal_login_form(self.request.user, context)
-        for i in self.queryset:
-            context[i.title] = Article.objects.filter(category=i)
-        return context
+def magazine(request):
+    magazin_category = get_object_or_404(Category, title='Magazin')
+    context = {sub.title: sub.article_set.all() for sub in magazin_category.get_descendants()}
+    return render(request, 'front_magazine.html', context)
 
 
 class Forum(ListView):
     model = Article
     template_name = "front_forum.html"
-    queryset = Article.objects.filter(category=Category.objects.get(pk=3))[:20]
+    queryset = Article.objects.filter(category=Category.objects.filter(title='Forum'))[:20]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
