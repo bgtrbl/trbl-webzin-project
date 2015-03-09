a9b1ab3d8fc8f812d66810363b466813d8a70093
Chan Cho <verakorvo@gmail.com>
2015-03-09 12:30:23 -0700

    favicon.ico, robots.txt 업데이트(AWS initial deploy)

--- bgtrbl/urls.py
+++ bgtrbl/urls.py
@@ -1,15 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+
 from django.contrib.auth.decorators import login_required
+
+from django.views.generic.base import RedirectView
+from django.http import HttpResponse
 
 from bgtrbl.apps.userprofile.views import editUserProfile, updateUserProfile, myUserProfile,  UserProfileDetail
 
+# @todo fix this
 from .settings import MEDIA_ROOT
 
 
 # @todo profile 관련 main 으로
 urlpatterns = patterns('',
     url(r'^$', 'bgtrbl.main.views.home', name='home'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^main/', include('bgtrbl.main.urls', namespace='main')),
     url(r'^trblcms/', include('bgtrbl.apps.trblcms.urls', namespace='trblcms')),
@@ -20,5 +26,6 @@ urlpatterns = patterns('',
     url(r'^ckeditor/', include('ckeditor.urls')),
     url(r'^search/', include('haystack.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
+    url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
 )
-# temporary media url
+# temporary media url 나중에 지워야함
