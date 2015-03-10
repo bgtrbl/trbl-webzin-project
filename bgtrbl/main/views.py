053aea3a6fd99b7029fe243979736a56d2a9d13d
Chan Cho <verakorvo@gmail.com>
2015-03-09 17:17:30 -0700

    home design : 그냥 그림 몇개 추가

--- bgtrbl/main/views.py
+++ bgtrbl/main/views.py
@@ -17,9 +17,17 @@ def home(request):
     context = dict()
 
     # 이미지 임시 홀더 (design_comps/img)
-    import os
-    IMG_DIR = os.path.join(settings.MEDIA_ROOT,'design_comps','img')
-    context['images']=["/media/design_comps/img/{}".format(_) for _ in os.listdir(IMG_DIR) if not _.startswith(".")]
+    i_1 = ["chimney02.jpg", "copy_0_piu09.jpg", "hikarimachi01.jpg",
+    "hikarimachi10.jpg", "set01.jpg", "set04.jpg"]
+    i_2 = ["tumblr_lpp3nwMKeG1qzm9x1o1_500.jpg",
+    "tumblr_lqodmwu1Bc1qkajw3o1_500.jpg",
+    "tumblr_lsmept3KWX1qa8l9bo1_500.jpg", "tumblr_lta828HHqO1qau50i.jpg"]
+    i_3 = ["superthumb.gif", "tumblr_kwu8hsmk3Z1qzirnvo1_500.png",
+    "tumblr_kwu8hsmk3Z1qzirnvo1_500.png",
+    "tumblr_kx1nmq9ihk1qa4rvko1_400.png",
+    "tumblr_lmyk6v85ho1qla8s3o1_500.jpg"]
+
+    context['images'] = [ "/media/design_comps/img/{}".format(_) for _ in i_1 ]
 
     return render(request, 'main/home.html', context)
 
