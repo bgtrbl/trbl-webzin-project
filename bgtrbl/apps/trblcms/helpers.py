6fdf70ab4cccae678944a5dc349eb79045b75524
Chan Cho <verakorvo@gmail.com>
2015-03-10 15:08:48 -0700

    trblcms: add get_body_string method to Article model updated forum and magazin templates

---
+++ bgtrbl/apps/trblcms/helpers.py
@@ -0,0 +1,11 @@
+import re
+
+
+def get_visible_string(tags, join_char=" "):
+    def _visible(tag):
+        if tag.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+            return False
+        elif re.match('<!--.*-->', str(tag)):
+            return False
+        return True
+    return join_char.join(filter(_visible, tags))
